e626c6f5d6ea129b60d5f5049c95d70586e6ccee
unknown <hillelcoren@gmail.com>
2018-06-24 05:48:31 -0700

    Starter

--- starter.sh
+++ starter.sh
@@ -23,6 +23,7 @@ if [ ${action} = "init" ]; then
 
     # Replace 'hillelcoren'
     declare -a files=(
+        "./.packages"
         './ios/Runner.xcodeproj/project.pbxproj'
         './android/app/build.gradle'
         './android/app/src/main/AndroidManifest.xml'
@@ -67,7 +68,6 @@ if [ ${action} = "init" ]; then
     done
 
     declare -a files=(
-        "./.packages"
         "./pubspec.yaml"
         "./ios/Runner/Info.plist"
         "./ios/Flutter/Generated.xcconfig"
